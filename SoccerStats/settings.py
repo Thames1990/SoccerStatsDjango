aecf3f5002e42022576dead33d2c9c0b7fcb9093
Thames1990 <ThomasMohr1990@gmail.com>
2016-12-09 09:44:23 +0100

    Add STATIC_ROOT path

--- SoccerStats/settings.py
+++ SoccerStats/settings.py
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'SoccerStats/static')
 
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": lambda request: True,

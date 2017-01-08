ab308f275c580f92a61431c2bd2bf3b5c6f77316
Thames1990 <ThomasMohr1990@gmail.com>
2017-01-08 14:10:10 +0100

    Re-add favicon Add placeholder image for Player Add placeholder image for Team Small HTML changes

--- SoccerStats/settings.py
+++ SoccerStats/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
+# TODO Use environment variable
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'etz%bq#5yic%o$&y)jrgz_dm9u$p0b4cz%aeqfa7@+oq#jbop$'
 

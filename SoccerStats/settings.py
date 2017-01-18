9c1c44b5dd9aaf76485aa0ad37b5170fdc67d905
Thames1990 <ThomasMohr1990@gmail.com>
2017-01-18 21:00:13 +0100

    Fix static files error (maybe?)

--- SoccerStats/settings.py
+++ SoccerStats/settings.py
@@ -125,10 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'SoccerStats/static/SoccerStats/'),
-)
-STATIC_ROOT = "/home/django/SoccerStatsDjango/SoccerStats/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'SoccerStats/static/')
 
 # Django Debug Toolbar
 

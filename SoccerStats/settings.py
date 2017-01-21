fff88180d4cd161d0f6cab3335ff0116855205f3
Thames1990 <ThomasMohr1990@gmail.com>
2017-01-21 15:16:47 +0100

    Add postgres to installed apps

--- SoccerStats/settings.py
+++ SoccerStats/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.humanize',
     'django.contrib.messages',
+    'django.contrib.postgres',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
     'SoccerStats',

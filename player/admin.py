385e874ab268d932346e638fbff3e7931793d7be
Thames1990 <ThomasMohr1990@gmail.com>
2017-01-26 09:23:47 +0100

    Add AdminModel to Player model

--- player/admin.py
+++ player/admin.py
@@ -2,4 +2,48 @@ from django.contrib import admin
 
 from .models import Player
 
-admin.site.register(Player)
+
+class PlayerAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': [
+                'team',
+                'name',
+                'position',
+                'jersey_number',
+                'nationality',
+                'market_value',
+                'image',
+            ]
+        }),
+        ('Zeitangaben', {
+            'fields': [
+                'date_of_birth',
+                'contract_until',
+            ]
+        }),
+    ]
+    list_display = (
+        'team',
+        'name',
+        'position',
+        'jersey_number',
+        'date_of_birth',
+        'nationality',
+        'contract_until',
+        'market_value',
+        'image',
+    )
+    list_filter = (
+        'position',
+        'nationality',
+    )
+    search_fields = [
+        'team__name',
+        'name',
+        'position',
+        'nationality',
+    ]
+
+
+admin.site.register(Player, PlayerAdmin)

38355a0dfc3dd4738b985bb03a1cc68c67dd5c21
Thames1990 <ThomasMohr1990@gmail.com>
2017-02-13 07:55:24 +0100

    Add STATICFILES_DIRS

--- SoccerStats/settings.py
+++ SoccerStats/settings.py
@@ -100,6 +100,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticfiles"),
+)
 
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK": lambda request: True,

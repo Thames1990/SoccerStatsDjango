b0bf2b1cb4b57911686ca6cffb375f903072ab65
Thames1990 <ThomasMohr1990@gmail.com>
2017-02-13 08:14:07 +0100

    Stuff

--- SoccerStats/settings.py
+++ SoccerStats/settings.py
@@ -98,7 +98,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'SoccerStats/static'),
